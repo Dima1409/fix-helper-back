b6f70b4e1e8f96551e1e95015a6f3231970f36b2
Dima <57713733+Dima1409@users.noreply.github.com>
2024-04-23 22:40:53 +0300

    fix patterns

--- models/patterns.js
+++ models/patterns.js
@@ -5,11 +5,11 @@ const codePassPattern = /^(?!.*\s)(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d-]{6,20}$/;
 const steeringRackPattern = /^[a-zA-Z]{2}\d{3}$/;
 const rackKitPattern = /^[a-zA-Z]{2}\d{3}[kK][iI][tT]$/;
 const rackMorePattern = /^[a-zA-Z]{2}\d{3}[sS][pP][eE][cC]$/;
-const artPattern = /^[a-zA-Z0-9-]{3,20}$/;
+const artPattern = /^[a-zA-Z0-9\-.,/()]{3,22}$/;
 const quantityPattern = /^[a-zA-Z0-9-]{1,2}$/;
 const commentPattern = /^[a-zA-Z0-9-ґҐєЄіІїЇґҐєЄіІїЇ]{8,90}$/;
-const applicationPattern = /^[a-zA-Z0-9-]{8,400}$/;
-const oemPattern = /^[a-zA-Z0-9-]{8,800}$/;
+const applicationPattern = /^[a-zA-Z0-9\s\-,.\u0400-\u04FF]{8,800}$/;
+const oemPattern = /^[a-zA-Z0-9\s\-,.\u0400-\u04FF]{8,1400}$/;
 
 module.exports = {
   namePattern,
